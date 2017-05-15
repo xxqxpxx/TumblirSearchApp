acf991cba047109cd8d05e78a5a6f13146cacd9a
Ahmed Abdul Fatah <ahmedafatah@outlook.com>
2017-05-15 15:03:57 +0200

    sending the word from the main activity to the search activity

--- app/src/main/java/com/example/ahmed/tumblrsearchapi/MainActivity.java
+++ app/src/main/java/com/example/ahmed/tumblrsearchapi/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.ahmed.tumblrsearchapi;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    
+    public void searchbutton(View v)
+    {
+        EditText word = (EditText) findViewById(R.id.searchEntry);
+        Intent intent = new Intent(MainActivity.this, Search.class);
+        intent.putExtra("searchEntry", word.getText().toString() );
+        startActivity(intent);
     }
 }
